78314e419c13122e57922d7a5f7904326ad25c4a
Muhib Faiyaz <faiyazabidmuhib@gmail.com>
2026-06-29 01:48:18 +0600

    added score feature and a game over screen

--- Snake game/Source.cpp
+++ Snake game/Source.cpp
@@ -19,6 +19,17 @@ int main()
     sf::Vector2i food = { 10, 10 };
     bool alive = true;
 
+    int score = 0;  
+
+    sf::Font font;
+    font.openFromFile("arial.ttf");         
+
+    sf::Text scoreText(font);
+    scoreText.setCharacterSize(20);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setPosition(sf::Vector2f(10.f, 10.f));
+
+
     while (window.isOpen())
     {
         
@@ -59,13 +70,17 @@ int main()
             {
                 snake.push_front(newHead);
                 if (newHead == food)
+                {
                     food = { rand() % (W / CELL), rand() % (H / CELL) };
+                    score++;
+                }
                 else
                     snake.pop_back();
             }
         }
 
-        
+        scoreText.setString("Score: " + std::to_string(score));
+
         window.clear(sf::Color(30, 30, 30));
 
         sf::RectangleShape cell(sf::Vector2f(CELL , CELL ));
@@ -81,8 +96,35 @@ int main()
         cell.setPosition(sf::Vector2f(food.x * CELL, food.y * CELL));
         window.draw(cell);
 
+        window.draw(scoreText);
+
         if (!alive)
+        {
             window.clear(sf::Color(100, 0, 0));
+
+            sf::Text gameOverText(font);
+            gameOverText.setCharacterSize(60);
+            gameOverText.setFillColor(sf::Color::White);
+            gameOverText.setString("GAME OVER");
+
+            sf::FloatRect goBounds = gameOverText.getLocalBounds();
+            gameOverText.setOrigin(sf::Vector2f(
+                goBounds.position.x + goBounds.size.x / 2.f,
+                goBounds.position.y + goBounds.size.y / 2.f
+            ));
+            gameOverText.setPosition(sf::Vector2f(W / 2.f, H / 2.f - 50.f));
+
+
+            scoreText.setCharacterSize(40);
+            sf::FloatRect bounds = scoreText.getLocalBounds();        
+            scoreText.setOrigin(sf::Vector2f(                         
+                 bounds.position.x + bounds.size.x / 2.f,
+                bounds.position.y + bounds.size.y / 2.f
+            ));
+            scoreText.setPosition(sf::Vector2f(W/2.0f, H/2.0f));
+            window.draw(scoreText);
+            window.draw(gameOverText);
+        }
 
         window.display();
     }
